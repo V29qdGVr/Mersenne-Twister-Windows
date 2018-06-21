46e7043a12e826f0a64a27a565abd8e687615f76
Wojciech Kazior <kazior.wojciech@gmail.com>
2018-06-21 17:36:10 +0200

    Initial commit.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using Windows.UI.Notifications;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace MersenneTwister
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void GenerateAndSaveToFileAsync(uint seed)
+        {
+            MersenneTwister mersenneTwister = new MersenneTwister(seed);
+            byte[] tenMillionBytes = new byte[10000000];
+
+            for (uint i = 0; i < 2500000; i++)
+            {
+                uint generatedInteger = mersenneTwister.Random();
+                uint arrayIndex = i * 4;
+                this.InsertIntegerIntoByteArray(tenMillionBytes, generatedInteger, arrayIndex);
+
+                uint bytesGenerated = (i + 1) * 4;
+                if (bytesGenerated % 1000000 == 0)
+                {
+                    Debug.WriteLine(bytesGenerated + " bytes generated");
+                    this.ShowToastNotification("MersenneTwister", "The file is ready!");
+                }
+            }
+
+            Windows.Storage.StorageFolder storageFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
+            Windows.Storage.StorageFile file = await storageFolder.CreateFileAsync("output", Windows.Storage.CreationCollisionOption.ReplaceExisting);
+            File.WriteAllBytes(file.Path, tenMillionBytes);
+        }
+
+        private void InsertIntegerIntoByteArray(byte[] byteArray, uint integer, uint index)
+        {
+            byte[] integerBytes = BitConverter.GetBytes(integer);
+            byteArray[index + 0] = integerBytes[3];
+            byteArray[index + 1] = integerBytes[2];
+            byteArray[index + 2] = integerBytes[1];
+            byteArray[index + 3] = integerBytes[0];
+        }
+
+        private void ShowToastNotification(string title, string stringContent)
+        {
+            ToastNotifier ToastNotifier = ToastNotificationManager.CreateToastNotifier();
+            Windows.Data.Xml.Dom.XmlDocument toastXml = ToastNotificationManager.GetTemplateContent(ToastTemplateType.ToastText02);
+            Windows.Data.Xml.Dom.XmlNodeList toastNodeList = toastXml.GetElementsByTagName("text");
+            toastNodeList.Item(0).AppendChild(toastXml.CreateTextNode(title));
+            toastNodeList.Item(1).AppendChild(toastXml.CreateTextNode(stringContent));
+            Windows.Data.Xml.Dom.IXmlNode toastNode = toastXml.SelectSingleNode("/toast");
+            Windows.Data.Xml.Dom.XmlElement audio = toastXml.CreateElement("audio");
+            audio.SetAttribute("src", "ms-winsoundevent:Notification.SMS");
+
+            ToastNotification toast = new ToastNotification(toastXml);
+            toast.ExpirationTime = DateTime.Now.AddSeconds(4);
+            ToastNotifier.Show(toast);
+        }
+
+        private void Button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            var textBox = (button.Parent as StackPanel).Children[2] as TextBox;
+            uint seed = (uint)Int32.Parse(textBox.Text);
+            this.GenerateAndSaveToFileAsync(seed);
+        }
+    }
+}
---
+++ MersenneTwister.cs
@@ -0,0 +1,71 @@
+﻿namespace MersenneTwister
+{
+    class MersenneTwister
+    {
+        private const uint A =           0b10011001000010001011000011011111; // 2567483615 // 0x9908B0DF
+        private const uint B =           0b10011101001011000101011010000000; // 2636928640 // 0x9D2C5680
+        private const uint C =           0b11101111110001100000000000000000; // 4022730752 // 0xEFC60000
+        private const uint D =           0b11111111111111111111111111111111; // 4294967295 // 0xFFFFFFFF
+        private const uint F =           0b01101100000001111000100101100101; // 1812433253 // 0x6C078965
+        private const uint L =           0b00000000000000000000000000010010; // 0000000018 // 0x00000012
+        private const uint M =           0b00000000000000000000000110001101; // 0000000397 // 0x0000018D
+        private const uint N =           0b00000000000000000000001001110000; // 0000000624 // 0x00000270
+        private const uint R =           0b00000000000000000000000000011111; // 0000000031 // 0x0000001F
+        private const uint S =           0b00000000000000000000000000000111; // 0000000007 // 0x00000007
+        private const uint T =           0b00000000000000000000000000001111; // 0000000015 // 0x0000000F
+        private const uint U =           0b00000000000000000000000000001011; // 0000000011 // 0x0000000B
+        private const uint W =           0b00000000000000000000000000100000; // 0000000032 // 0x00000020
+        private const uint MASK_LOWER =  0b01111111111111111111111111111111; // 2147483647 // 0x7FFFFFFF
+        private const uint MASK_UPPER =  0b10000000000000000000000000000000; // 2147483648 // 0x80000000
+
+        private uint[] mt;
+        private uint index;
+
+        public MersenneTwister(uint seed)
+        {
+            mt = new uint[N];
+            index = N;
+            mt[0] = seed;
+
+            for (uint i = 1; i < N; i++)
+            {
+                mt[i] = (F * (mt[i - 1] ^ (mt[i - 1] >> 30)) + i);
+            }
+        }
+
+        public uint Random()
+        {
+            uint i = index;
+            if (index >= N)
+            {
+                this.Twist();
+                i = index;
+            }
+            uint y = mt[i];
+            index = i + 1;
+
+            y ^= (mt[i] >> (int)U);
+            y ^= (y << (int)S) & B;
+            y ^= (y << (int)T) & C;
+            y ^= (y >> (int)L);
+
+            return y;
+        }
+
+        private void Twist()
+        {
+            uint i, x, xA;
+            for (i = 0; i < N; i++)
+            {
+                x = (mt[i] & MASK_UPPER) + (mt[(i + 1) % N] & MASK_LOWER);
+                xA = x >> 1;
+                if ((x & 0x1) == 1)
+                {
+                    xA ^= A;
+                }
+                mt[i] = mt[(i + M) % N] ^ xA;
+            }
+            index = 0;
+        }
+    }
+}
